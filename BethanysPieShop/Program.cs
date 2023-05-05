823a27ab3c50d49d88dc7db5e88f9aca65c27e07
chawla971 <sahil971chawla@gmail.com>
2023-05-05 16:07:37 -0700

    Adding a Form page, with model binding and validation on server and client side

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository,PieRepository>();
+builder.Services.AddScoped<IOrderRepository,OrderRepository>();
 
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 builder.Services.AddSession();
